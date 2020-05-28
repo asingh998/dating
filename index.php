ef8306651348c546ad80d830cdde47e99cfdaafc
Arshdeep Singh <asingh69@mail.greenriver.edu>
2020-05-27 20:10:47 -0400

    added validation

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ error_reporting(E_ALL);
 //Require the autoload file
 require_once("vendor/autoload.php");
 require_once ("model/data-layer.php");
+require_once("model/data-validation.php");
 
 //Instantiate the F3 Base class
 $f3 = Base::instance();
@@ -31,6 +32,30 @@ $f3->route('GET|POST /personal', function ($f3){
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         //var_dump($_POST);
 
+        //Validate name
+        if (!validName($_POST['first'])) {
+
+            //Set an error variable in the F3 hive
+            $f3->set('errors["first"]', "Invalid name");
+        }
+        if (!validLname($_POST['last'])) {
+
+            //Set an error variable in the F3 hive
+            $f3->set('errors["last"]', "Invalid name");
+        }
+        if (!validPhone($_POST['phone'])) {
+
+            //Set an error variable in the F3 hive
+            $f3->set('errors["phone"]', "Invalid Phone number");
+        }
+        if (!validAge($_POST['age'])) {
+
+            //Set an error variable in the F3 hive
+            $f3->set('errors["age"]', "Invalid age");
+        }
+        //Data is valid
+        else {
+        if(empty($f3->get('errors'))){
             //Store the data in the session array
             $_SESSION['first'] = $_POST['first'];
             $_SESSION['last'] = $_POST['last'];
@@ -40,10 +65,13 @@ $f3->route('GET|POST /personal', function ($f3){
 
             //Redirect to Order 2 page
             $f3->reroute('profile');
-
+        }}
     }
 
-    //$f3->set('first', $_POST['first']);
+    $f3->set('first', $_POST['first']);
+    $f3->set('last', $_POST['last']);
+    $f3->set('age', $_POST['age']);
+    $f3->set('phone', $_POST['phone']);
     $f3->set('gender', $gender);
     $views = new Template();
     echo $views->render('views/personal.html');
@@ -57,15 +85,24 @@ $f3->route('GET|POST /profile', function($f3){
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         //var_dump($_POST);
 
-        //Store the data in the session array
-       $_SESSION['email'] = $_POST['email'];
-       $_SESSION['state'] = $_POST['state'];
-       $_SESSION['seek'] = $_POST['seek'];
-       $_SESSION['bio'] = $_POST['bio'];
+        //Validate email
+        if (!validEmail($_POST['email'])) {
+            //Set an error variable in the F3 hive
+            $f3->set('errors["email"]', "Invalid email");
+        }
 
-        //Redirect to Order 2 page
-        $f3->reroute('interest');
+        //Data is valid
+        else{
+        if (empty($f3->get('errors'))) {
+            //Store the data in the session array
+            $_SESSION['email'] = $_POST['email'];
+            $_SESSION['state'] = $_POST['state'];
+            $_SESSION['seek'] = $_POST['seek'];
+            $_SESSION['bio'] = $_POST['bio'];
 
+            //Redirect to Order 2 page
+            $f3->reroute('interest');
+        }}
     }
 
     $f3->set('gender', $gender);
@@ -81,12 +118,27 @@ $f3->route('GET|POST /interest', function($f3){
     //If the form has been submitted
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        //Store the data in the session array
-        $_SESSION['interests'] = $_POST['interests'];
-        $_SESSION['outinterests'] = $_POST['outinterests'];
-        //Redirect to summary page
-        $f3->reroute('summary');
+        if (!validIndoor($_POST['interests'])) {
 
+            //Set an error variable in the F3 hive
+            $f3->set('errors["interests"]', "Invalid interest.");
+        }
+        if (!validOutdoor($_POST['outinterests'])) {
+
+            //Set an error variable in the F3 hive
+            $f3->set('errors["interests"]', "Invalid interest.");
+        }
+        //Data is valid
+        else {
+        if (empty($f3->get('errors'))) {
+
+            //Store the data in the session array
+            $_SESSION['interests'] = $_POST['interests'];
+            $_SESSION['outinterests'] = $_POST['outinterests'];
+
+            //Redirect to summary page
+            $f3->reroute('summary');
+        }}
     }
 
     $f3->set('interests', $interests);
